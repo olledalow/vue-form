e4fb0f2b25f134d427e52cf8fb2ee6742a91709d
Bence Olle <bence.olle.job@gmail.com>
2020-10-05 22:21:14 +0200

    connected form input data with vue, and added custom input (rating)

---
+++ src/components/RatingControl.vue
@@ -0,0 +1,57 @@
+<template>
+  <ul>
+    <li :class="{ active: modelValue === 'poor' }">
+      <button @click="activate('poor')" type="button">Poor</button>
+    </li>
+    <li :class="{ active: modelValue === 'average' }">
+      <button @click="activate('average')" type="button">Average</button>
+    </li>
+    <li :class="{ active: modelValue === 'great' }">
+      <button @click="activate('great')" type="button">Great</button>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['modelValue'],
+  emits: ['update:modelValue'],
+  methods: {
+    activate(option) {
+      this.$emit('update:modelValue', option);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.active {
+  border-color: rgb(180, 0, 180);
+}
+.active button {
+  color: rgb(180, 0, 180);
+}
+
+ul {
+  list-style: none;
+  margin: 0.5rem 0;
+  padding: 0;
+  display: flex;
+}
+
+li {
+  margin: 0 1rem;
+  border: 1px solid #ccc;
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+button {
+  font: inherit;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+}
+</style>
